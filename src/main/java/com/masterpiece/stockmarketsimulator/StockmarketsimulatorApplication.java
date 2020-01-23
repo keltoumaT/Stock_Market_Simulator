c1279817e26db082471762f55a07dcc10dbd5f83
keltoumaT <keltouma.tahi@gmail.com>
2020-01-23 11:05:55 +0100

    added spring initializer

---
+++ src/main/java/com/masterpiece/stockmarketsimulator/StockmarketsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.masterpiece.stockmarketsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmarketsimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockmarketsimulatorApplication.class, args);
+    }
+
+}
