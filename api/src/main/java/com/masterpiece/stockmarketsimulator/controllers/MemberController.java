1ea0cd0b23fa4d53e6cce478d39fd0e4fc994172
keltoumaT <keltouma.tahi@gmail.com>
2020-02-01 22:51:59 +0100

    updated gitignore

--- api/src/main/java/com/masterpiece/stockmarketsimulator/controllers/MemberController.java
+++ api/src/main/java/com/masterpiece/stockmarketsimulator/controllers/MemberController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
