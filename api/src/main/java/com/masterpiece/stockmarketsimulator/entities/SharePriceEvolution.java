44033dbb467a4e10fab044c868db67e3ad32f9fc
keltoumaT <keltouma.tahi@gmail.com>
2020-02-29 17:17:34 +0100

    modified url in CompanyService

--- api/src/main/java/com/masterpiece/stockmarketsimulator/entities/SharePriceEvolution.java
+++ api/src/main/java/com/masterpiece/stockmarketsimulator/entities/SharePriceEvolution.java
@@ -1,4 +1,92 @@
 package com.masterpiece.stockmarketsimulator.entities;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.LocalDate;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SharePriceEvolution {
+
+
+    private LocalDate date;
+
+    private double open;
+
+    private double close;
+
+    private double high;
+
+    private double low;
+
+    private Long volume;
+
+    private double changePercent;
+
+    private String label;
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getOpen() {
+        return open;
+    }
+
+    public void setOpen(double open) {
+        this.open = open;
+    }
+
+    public double getClose() {
+        return close;
+    }
+
+    public void setClose(double close) {
+        this.close = close;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public Long getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Long volume) {
+        this.volume = volume;
+    }
+
+    public double getChangePercent() {
+        return changePercent;
+    }
+
+    public void setChangePercent(double changePercent) {
+        this.changePercent = changePercent;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
